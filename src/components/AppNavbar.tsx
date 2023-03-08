8d240504cda8873ca18bcd4f179e401a15476433
Kar Rui Lau <karrui@open.gov.sg>
2023-03-08 20:31:54 +0800

    feat: add AppNavbar to main page layout

---
+++ src/components/AppNavbar.tsx
@@ -0,0 +1,50 @@
+import { Flex, HStack, useDisclosure } from '@chakra-ui/react';
+import {
+  AvatarMenu,
+  AvatarMenuDivider,
+  Link,
+  Menu,
+} from '@opengovsg/design-system-react';
+import { signOut, useSession } from 'next-auth/react';
+import Image from 'next/image';
+import NextLink from 'next/link';
+
+import ogpLogo from '~/assets/ogp-logo.svg';
+import { EditProfileModal } from '~/features/profile';
+
+export const AppNavbar = (): JSX.Element => {
+  const { data: session } = useSession();
+  const { onOpen, isOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <EditProfileModal isOpen={isOpen} onClose={onClose} />
+      <Flex
+        justify="space-between"
+        align="center"
+        px={{ base: '1.5rem', md: '1.8rem', xl: '2rem' }}
+        py="0.75rem"
+        bg="white"
+        borderBottomWidth="1px"
+      >
+        <Link as={NextLink} title="Form Logo" href="/">
+          <Image src={ogpLogo} alt="OGP Logo" priority />
+        </Link>
+        <HStack
+          textStyle="subhead-1"
+          spacing={{ base: '0.75rem', md: '1.5rem' }}
+        >
+          <AvatarMenu
+            src={session?.user?.image ?? undefined}
+            name={session?.user?.email ?? undefined}
+            menuListProps={{ maxWidth: '19rem' }}
+          >
+            <Menu.Item onClick={onOpen}>Edit profile</Menu.Item>
+            <AvatarMenuDivider />
+            <Menu.Item onClick={() => signOut()}>Sign out</Menu.Item>
+          </AvatarMenu>
+        </HStack>
+      </Flex>
+    </>
+  );
+};
