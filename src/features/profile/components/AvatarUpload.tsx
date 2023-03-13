ace650cbb49a7dec7141518cb0f3c59aad27eef7
Kar Rui Lau <karrui@open.gov.sg>
2023-03-14 04:31:16 +0800

    fix: lint

--- src/features/profile/components/AvatarUpload.tsx
+++ src/features/profile/components/AvatarUpload.tsx
@@ -29,7 +29,7 @@ export const AvatarUpload = ({ url }: AvatarUploadProps): JSX.Element => {
 
     return uploadAvatarMutation.mutateAsync(file, {
       onSuccess: async (newPath) => {
-        setCurrentAvatarUrl(newPath);
+        setCurrentAvatarUrl(newPath ?? '');
         toast({
           description: 'Avatar uploaded successfully.',
         });
