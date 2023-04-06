41a7948f1f53621dc4ad3ac0efdc9e44054435e5
Kar Rui Lau <karrui@open.gov.sg>
2023-04-06 16:11:54 +0800

    fix: update smoke test for new text

--- playwright/smoke.test.ts
+++ playwright/smoke.test.ts
@@ -5,7 +5,7 @@ test.setTimeout(35e3)
 test('go to /', async ({ page }) => {
   await page.goto('/')
 
-  await page.waitForSelector(`text=Vibes`)
+  await page.waitForSelector(`text=OGP Starter Kit`)
 })
 
 test('test 404', async ({ page }) => {
