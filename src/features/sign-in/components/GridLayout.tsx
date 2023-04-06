42cdafb455cbfc6afb24b433ccdffe66c6117c6b
Kar Rui Lau <karrui@open.gov.sg>
2023-04-06 15:36:50 +0800

    feat: update sign in page theme

--- src/features/sign-in/components/GridLayout.tsx
+++ src/features/sign-in/components/GridLayout.tsx
@@ -12,10 +12,10 @@ export const BackgroundBox: FC<PropsWithChildren> = ({ children }) => (
     h="inherit"
     minH="$100vh"
     bgGradient={{
-      md: 'linear(to-b, brand.primary.500 20.5rem, white 0)',
+      md: 'linear(to-b, base.canvas.brand-subtle 20.5rem, white 0)',
       // We use 41.6667% because at the lg size, the grid column of the login area
       // is 7/12, so the remaining space is 5/12 = 0.416666...
-      lg: 'linear(to-r, brand.primary.500 calc(41.6667% - 4px), white 0)',
+      lg: 'linear(to-r, base.canvas.brand-subtle calc(41.6667% - 4px), white 0)',
     }}
   >
     {children}
@@ -43,10 +43,9 @@ export const LoginGridArea: FC<PropsWithChildren> = ({ children }) => (
 // Grid area styling for the footer.
 export const FooterGridArea: FC<PropsWithChildren> = ({ children }) => (
   <GridItem
-    alignSelf="end"
-    gridColumn={{ base: '1 / 5', md: '2 / 12' }}
-    pb={{ base: 0, lg: '2.5rem' }}
-    bg={{ base: 'base.canvas.brandLight', lg: 'transparent' }}
+    gridColumn={{ base: '1 / 5', md: '2 / 12', lg: '7 / 12' }}
+    py="4rem"
+    display="flex"
   >
     {children}
   </GridItem>
@@ -58,12 +57,12 @@ export const NonMobileSidebarGridArea: FC<PropsWithChildren> = ({
 }) => (
   <GridItem
     display={{ base: 'none', md: 'flex' }}
-    gridColumn={{ md: '1 / 13', lg: '2 / 6' }}
+    gridColumn={{ md: '1 / 13', lg: '1 / 6' }}
     h={{ md: '20.5rem', lg: 'auto' }}
     pt={{ base: '1.5rem', md: '2.5rem', lg: '3rem' }}
     pb={{ lg: '3rem' }}
     flexDir="column"
-    alignItems={{ base: 'center', lg: 'flex-end' }}
+    alignItems="center"
     justifyContent="center"
   >
     {children}
