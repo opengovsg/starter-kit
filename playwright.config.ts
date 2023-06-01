9fe0c392e2a3071504fa96efd478c025bdfa6213
Kar Rui Lau <karrui@open.gov.sg>
2023-06-01 15:54:19 +0800

    feat: use dotenv to load env vars for e2e instead of nextjs helper
    consistency i guess, and less hidden

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,5 @@
 import { type PlaywrightTestConfig, devices } from '@playwright/test'
 
-import { loadEnvConfig } from '@next/env'
-
-// Load environment variables just like Next.js does.
-// Will load from `.env.test` due to the `NODE_ENV=test` environment variable in `test:e2e` script.
-loadEnvConfig(process.cwd())
-
 const baseUrl = process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:3000'
 console.log(`ℹ️ Using base URL "${baseUrl}"`)
 
