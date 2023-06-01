d7d82de12cf2ae1b12d6491e6b819197252d089d
Austin Woon Quan <43132101+austinwoon@users.noreply.github.com>
2023-06-01 15:13:53 +0800

    chore: pr comments

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,17 +1,23 @@
 import winston, { format } from 'winston'
 
-const { combine, timestamp, prettyPrint } = format
+const { combine, timestamp, prettyPrint, colorize } = format
 
 export const createBaseLogger = (path: string) => {
   const formats = [
     winston.format.json(),
     timestamp(),
     process.env.NODE_ENV === 'development' ? prettyPrint() : undefined,
+    process.env.NODE_ENV === 'development' ? colorize() : undefined,
   ].filter((v) => !!v) as winston.Logform.Format[]
 
   return winston.createLogger({
     format: combine(...formats),
-    transports: [new winston.transports.Console()],
+    exitOnError: false,
+    transports: [
+      new winston.transports.Console({
+        silent: process.env.NODE_ENV === 'test',
+      }),
+    ],
     defaultMeta: {
       path,
     },
