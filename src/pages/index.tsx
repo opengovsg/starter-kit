72e927cd81627cab6d83f8276c7aa83b156392d6
Austin Woon Quan <43132101+austinwoon@users.noreply.github.com>
2023-06-01 17:52:28 +0800

    chore: undo remove getssprops

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,29 @@
 import { Box } from '@chakra-ui/react'
 
+import { withSessionSsr } from '~/lib/withSession'
+
 // TODO: Will be landing page in the future, now just a redirect to appropriate page.
 const Index = () => {
   return <Box />
 }
+
+export const getServerSideProps = withSessionSsr(({ req }) => {
+  const user = req.session.user
+  if (user) {
+    return {
+      redirect: {
+        destination: '/dashboard',
+      },
+      props: {},
+    }
+  }
+
+  return {
+    redirect: {
+      destination: '/sign-in',
+    },
+    props: {},
+  }
+})
 
 export default Index
