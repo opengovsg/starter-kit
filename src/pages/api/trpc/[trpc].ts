5b236f74d72c0397a92f9a09403622fbe4252317
Austin Woon Quan <43132101+austinwoon@users.noreply.github.com>
2023-06-02 14:38:22 +0800

    chore: undo client side error handler

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -2,7 +2,6 @@
  * This file contains tRPC's HTTP response handler
  */
 import * as trpcNext from '@trpc/server/adapters/next'
-import { createBaseLogger } from '~/lib/logger'
 import { createContext } from '~/server/context'
 import { appRouter } from '~/server/modules/_app'
 
@@ -15,14 +14,10 @@ export default trpcNext.createNextApiHandler({
   /**
    * @link https://trpc.io/docs/error-handling
    */
-  onError({ error, path }) {
-    // This is logger re-creation is needed because trpc does not know if the logger middleware has been reached up till this point
-    // path can also be undefined if any error was thrown before that
-    const logger = createBaseLogger(path ?? 'no path found')
-
+  onError({ error }) {
     if (error.code === 'INTERNAL_SERVER_ERROR') {
       // send to bug reporting
-      logger.error('Something went wrong', error)
+      console.error('Something went wrong', error)
     }
   },
   /**
