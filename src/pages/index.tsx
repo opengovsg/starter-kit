d0d6e7cf72baa3511a8e1dc892ba7d783d4ca3dd
Austin Woon Quan <43132101+austinwoon@users.noreply.github.com>
2023-06-05 20:39:54 +0800

    docs: mention error boundary

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { useUser } from '~/features/profile/api'
 // TODO: Will be landing page in the future, now just a redirect to appropriate page.
 const Index = () => {
   // On navigating to index, Users will be redirected to the /sign-in if they are not logged in, and /dashboard if they are
+  // This is handled by the top level error boundary in the app root
   useUser({ redirectIfFound: true, redirectTo: '/dashboard' })
 
   return <Box />
