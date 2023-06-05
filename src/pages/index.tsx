5091812776fb2e049698c35635a33ca7e50d0d3d
Austin Woon Quan <43132101+austinwoon@users.noreply.github.com>
2023-06-05 11:16:11 +0800

    chore: add comments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { useUser } from '~/features/profile/api'
 
 // TODO: Will be landing page in the future, now just a redirect to appropriate page.
 const Index = () => {
+  // On navigating to index, Users will be redirected to the /sign-in if they are not logged in, and /dashboard if they are
   useUser({ redirectIfFound: true, redirectTo: '/dashboard' })
 
   return <Box />
