eca634960937b0af0ba3bf352045f7a4a19a6c66
Kar Rui Lau <karrui@open.gov.sg>
2023-06-07 14:51:32 +0800

    refactor: simplify AppPublicHeader

--- src/features/landing/components/AppPublicHeader.tsx
+++ src/features/landing/components/AppPublicHeader.tsx
@@ -1,4 +1,4 @@
-import { type As, Flex, type FlexProps, HStack } from '@chakra-ui/react'
+import { Flex, HStack, type As } from '@chakra-ui/react'
 import {
   Button,
   IconButton,
@@ -16,11 +16,9 @@ type PublicHeaderLinkProps = {
   MobileIcon?: As
 }
 
-export interface PublicHeaderProps {
+export interface AppPublicHeaderProps {
   /** Header links to display, if provided. */
   publicHeaderLinks?: PublicHeaderLinkProps[]
-  /** Call to action element to render, if any. */
-  ctaElement?: React.ReactElement
 }
 
 const PublicHeaderLink = ({
@@ -61,63 +59,30 @@ const PublicHeaderLink = ({
   )
 }
 
-export const PublicHeader = ({
+export const AppPublicHeader = ({
   publicHeaderLinks,
-  ctaElement: ctaButton,
-}: PublicHeaderProps): JSX.Element => {
-  return (
-    <PublicHeader.Container>
-      <NextLink title="Form Logo" href="https://form.gov.sg/">
-        Starter Kit
-      </NextLink>
-      <HStack
-        textStyle="subhead-1"
-        spacing={{ base: '1rem', md: '2rem', xl: '2.5rem' }}
-      >
-        {publicHeaderLinks?.map((link, index) => (
-          <PublicHeaderLink key={index} {...link} />
-        ))}
-        {ctaButton ?? null}
-      </HStack>
-    </PublicHeader.Container>
-  )
-}
-
-interface PublicHeaderContainerProps extends FlexProps {
-  children: React.ReactNode
-}
-
-PublicHeader.Container = ({
-  children,
-  ...props
-}: PublicHeaderContainerProps): JSX.Element => {
-  return (
-    <Flex
-      gridColumn={{ base: '1 / -1', md: '2 / 12' }}
-      justify="space-between"
-      align="center"
-      // px={{ base: '1.5rem', md: '5.5rem', lg: '9.25rem' }}
-      py={{ base: '0.625rem', md: '4.5rem' }}
-      {...props}
-    >
-      {children}
-    </Flex>
-  )
-}
-
-export const AppPublicHeader = (): JSX.Element => {
+}: AppPublicHeaderProps): JSX.Element => {
   return (
     <AppGrid bg="base.canvas.brand-subtle">
-      <PublicHeader
-        ctaElement={
+      <Flex
+        gridColumn={{ base: '1 / -1', md: '2 / 12' }}
+        justify="space-between"
+        align="center"
+        py={{ base: '0.625rem', md: '4.5rem' }}
+      >
+        <NextLink href="/">Starter Kit</NextLink>
+        <HStack
+          textStyle="subhead-1"
+          spacing={{ base: '1rem', md: '2rem', xl: '2.5rem' }}
+        >
+          {publicHeaderLinks?.map((link, index) => (
+            <PublicHeaderLink key={index} {...link} />
+          ))}
           <Button variant="solid" as={NextLink} href={SIGN_IN}>
             Log in
           </Button>
-        }
-      />
+        </HStack>
+      </Flex>
     </AppGrid>
   )
 }
-
-// @ts-expect-error displayName setting
-PublicHeader.Container.displayName = 'PublicHeader.Container'
