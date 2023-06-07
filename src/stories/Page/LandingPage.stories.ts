022183927e07d3356580761a953472ca9c041a2c
Kar Rui Lau <karrui@open.gov.sg>
2023-06-07 18:25:30 +0800

    feat: remove unneeded msw handler in landing page

--- src/stories/Page/LandingPage.stories.ts
+++ src/stories/Page/LandingPage.stories.ts
@@ -1,9 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { TRPCError } from '@trpc/server'
 import LandingPage from '~/pages/index'
 import { getMobileViewParameters } from '../utils/viewports'
-import { mockTrpcErrorResponse, trpcMsw } from '../__mocks__/trpc'
 
 const meta: Meta<typeof LandingPage> = {
   title: 'Pages/Landing Page',
@@ -11,18 +9,6 @@ const meta: Meta<typeof LandingPage> = {
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'fullscreen',
-    msw: {
-      handlers: [
-        trpcMsw.me.get.query((_req, res, ctx) => {
-          return res(
-            ctx.status(401),
-            ctx.json(
-              mockTrpcErrorResponse(new TRPCError({ code: 'UNAUTHORIZED' }))
-            )
-          )
-        }),
-      ],
-    },
   },
 }
 
