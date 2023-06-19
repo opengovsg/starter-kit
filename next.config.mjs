ea47f3fe8fe67ba99bb997547d28d1295d98f413
Kar Rui Lau <karrui@open.gov.sg>
2023-06-19 16:58:46 +0800

    feat: add default security headers to next.config.js
    Following helmet defaults

--- next.config.mjs
+++ next.config.mjs
@@ -24,6 +24,48 @@ const config = {
   images: {
     domains: [process.env.R2_PUBLIC_HOSTNAME ?? ''].filter((d) => d),
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "default-src 'self';base-uri 'self';font-src 'self' https: data:;form-action 'self';frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests",
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Cross-Origin-Resource-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Origin-Agent-Cluster',
+            value: '?1',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'no-referrer',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '0',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default config
