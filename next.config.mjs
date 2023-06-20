666e8dc4d014ef08478a9ecabb4baf7b3482d006
Kar Rui Lau <karrui@open.gov.sg>
2023-06-20 11:53:01 +0800

    feat: use next-safe package over self-declaration

--- next.config.mjs
+++ next.config.mjs
@@ -3,52 +3,9 @@
  * for Docker builds.
  */
 const { env } = await import('./src/env.mjs')
+import { nextSafe } from 'next-safe'
 
-// CSP for preview environment specifically, so as to enable Vercel workflow collaboration features.
-const PreviewCsp = (() => {
-  const isPreview =
-    process.env.VERCEL_ENV && process.env.VERCEL_ENV !== 'production'
-  if (!isPreview)
-    return {
-      scriptSrc: '',
-      connectSrc: '',
-      imgSrc: '',
-      frameSrc: '',
-    }
-  return {
-    // Note unsafe-inline. This should only be used in preview environments.
-    scriptSrc: "https://vercel.live/ https://vercel.com 'unsafe-inline'",
-    connectSrc:
-      'https://vercel.live/ https://vercel.com https://sockjs-us3.pusher.com/ wss://ws-us3.pusher.com/',
-    imgSrc:
-      'https://assets.vercel.com/ https://vercel.live/ https://vercel.com https://sockjs-us3.pusher.com/ data: blob:',
-    frameSrc: 'https://vercel.live/ https://vercel.com',
-  }
-})()
-
-const ContentSecurityPolicy = `
-  default-src 'self';
-  base-uri 'self';
-  font-src 'self' https: data:;
-  form-action 'self';
-  frame-ancestors 'self';
-  img-src 'self' data: blob: ${
-    // For displaying images from R2
-    env.R2_PUBLIC_HOSTNAME ? `https://${env.R2_PUBLIC_HOSTNAME}` : ''
-  } ${PreviewCsp.imgSrc};
-  frame-src 'self' ${PreviewCsp.frameSrc};
-  object-src 'none';
-  script-src 'self' ${env.NODE_ENV === 'development' ? "'unsafe-eval'" : ''} ${
-  PreviewCsp.scriptSrc
-};
-  style-src 'self' https: 'unsafe-inline';
-  connect-src 'self' https://*.browser-intake-datadoghq.com https://vitals.vercel-insights.com/v1/vitals ${
-    // For POSTing presigned URLs to R2 storage.
-    env.R2_S3_CSP_PATTERN || ''
-  } ${PreviewCsp.connectSrc};
-  worker-src 'self' blob:;
-  upgrade-insecure-requests
-`
+const isDev = env.NODE_ENV !== 'production'
 
 /**
  * @link https://nextjs.org/docs/api-reference/next.config.js/introduction
@@ -72,37 +29,26 @@ const config = {
   async headers() {
     return [
       {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+        source: '/:path*',
+        headers: nextSafe({
+          isDev,
+          contentSecurityPolicy: {
+            mergeDefaultDirectives: true,
+            'img-src': [
+              'data:',
+              'blob:',
+              // For displaying images from R2
+              env.R2_PUBLIC_HOSTNAME ? `https://${env.R2_PUBLIC_HOSTNAME}` : '',
+            ],
+            'style-src': ["'unsafe-inline'", 'https:'],
+            'connect-src': [
+              'https://*.browser-intake-datadoghq.com',
+              'https://vitals.vercel-insights.com/v1/vitals',
+              // For POSTing presigned URLs to R2 storage.
+              env.R2_S3_CSP_PATTERN || '',
+            ],
           },
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
-          },
-          {
-            key: 'Cross-Origin-Resource-Policy',
-            value: 'same-origin',
-          },
-          {
-            key: 'Origin-Agent-Cluster',
-            value: '?1',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
-          },
-        ],
+        }),
       },
     ]
   },
