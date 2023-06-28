522cb82e0ff6131ef55bc29edbc21dcbb8c10579
Kar Rui Lau <karrui@open.gov.sg>
2023-06-28 15:43:00 +0800

    fix: correctly mock trpc-msw with superjson

--- src/stories/utils/mockTrpc.ts
+++ src/stories/utils/mockTrpc.ts
@@ -1,13 +1,18 @@
 import { type TRPCError } from '@trpc/server'
 import { getHTTPStatusCodeFromError } from '@trpc/server/http'
 import { TRPC_ERROR_CODES_BY_KEY } from '@trpc/server/rpc'
-import { createTRPCMsw } from 'msw-trpc'
 import { type AppRouter } from '~/server/modules/_app'
 import { getBaseUrl } from '~/utils/getBaseUrl'
+import superjson from 'superjson'
+import { createTRPCMsw } from 'msw-trpc'
 
 export const trpcMsw = createTRPCMsw<AppRouter>({
   basePath: '/api/trpc',
   baseUrl: getBaseUrl(),
+  transformer: {
+    input: superjson,
+    output: superjson,
+  },
 })
 
 export const mockTrpcErrorResponse = (error: TRPCError, path?: string) => {
