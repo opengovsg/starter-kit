f941219f54ee23c3d634cf1fd342dbf111d259ce
Kar Rui Lau <karrui@open.gov.sg>
2023-07-13 17:00:38 +0800

    feat: update empty post text

--- src/features/home/components/EmptyPostList.tsx
+++ src/features/home/components/EmptyPostList.tsx
@@ -4,7 +4,7 @@ import { BusStop } from '~/components/Svg'
 export const EmptyPostList = (): JSX.Element => {
   return (
     <Stack spacing="2rem" align="center" pt="3rem">
-      <Text textStyle="subhead-2">No posts yet</Text>
+      <Text textStyle="subhead-2">There aren&apos;t any posts yet</Text>
       <BusStop />
     </Stack>
   )
