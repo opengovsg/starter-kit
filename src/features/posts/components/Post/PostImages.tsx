2fda0ffdc174f002c291a8b79af14d6e7c229249
Kar Rui Lau <karrui@open.gov.sg>
2023-07-24 13:35:55 +0800

    feat: fix image sizing according to design

--- src/features/posts/components/Post/PostImages.tsx
+++ src/features/posts/components/Post/PostImages.tsx
@@ -1,4 +1,5 @@
-import { Image, SimpleGrid } from '@chakra-ui/react'
+import { Box, Image, SimpleGrid } from '@chakra-ui/react'
+import { useMemo } from 'react'
 import { type RouterOutput } from '~/utils/trpc'
 
 export interface PostImagesProps {
@@ -6,6 +7,11 @@ export interface PostImagesProps {
 }
 
 export const PostImages = ({ images }: PostImagesProps): JSX.Element | null => {
+  const imageContainerHeight = useMemo(() => {
+    // Height = 16 rem if below 2 images, 8 rem if above 2 images
+    return images.length <= 2 ? '16rem' : '8rem'
+  }, [images.length])
+
   if (images.length === 0) {
     return null
   }
@@ -14,18 +20,19 @@ export const PostImages = ({ images }: PostImagesProps): JSX.Element | null => {
     <SimpleGrid
       py="0.5rem"
       // Clamp the number of columns between 1 and 2
-      columns={Math.min(Math.max(images.length, 1), 4)}
-      spacing="1.5rem"
+      columns={Math.min(Math.max(images.length, 1), 2)}
+      spacing="2px"
     >
       {images.map((url, index) => (
-        <Image
-          data-value="post-action"
-          key={index}
-          alt="Uploaded image"
-          src={url}
-          w="100%"
-          h="auto"
-        />
+        <Box height={imageContainerHeight} data-value="post-action" key={index}>
+          <Image
+            alt="Uploaded image"
+            src={url}
+            h="100%"
+            w="100%"
+            objectFit="cover"
+          />
+        </Box>
       ))}
     </SimpleGrid>
   )
