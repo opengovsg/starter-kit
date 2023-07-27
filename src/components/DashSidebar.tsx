4d4ba8902c6e9171aa6ffdc90c4e9cbae02aa77e
Kar Rui Lau <karrui@open.gov.sg>
2023-07-27 15:25:23 +0800

    feat: remove logout button on side dashbar

--- src/components/DashSidebar.tsx
+++ src/components/DashSidebar.tsx
@@ -7,14 +7,14 @@ import {
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useMemo } from 'react'
-import { BiFace, BiHomeSmile, BiLogOutCircle } from 'react-icons/bi'
+import { BiFace, BiHomeSmile } from 'react-icons/bi'
 import { ADMIN_DASHBAR_WIDTHS, ADMIN_NAVBAR_HEIGHT } from '~/constants/layouts'
 import { useMe } from '~/features/me/api'
 import { HOME, PROFILE, SETTINGS_PROFILE } from '~/lib/routes'
 
 export const DashSidebar = () => {
   const isMobile = useIsMobile()
-  const { me, logout } = useMe()
+  const { me } = useMe()
   const { pathname, query } = useRouter()
 
   const isProfileActive = useMemo(() => {
@@ -62,19 +62,6 @@ export const DashSidebar = () => {
             borderRadius={{ base: 0, md: 'md' }}
           >
             {isMobile ? '' : 'Profile'}
-          </SidebarItem>
-        </SidebarContainer>
-        <SidebarContainer size="sm">
-          <SidebarItem
-            icon={BiLogOutCircle}
-            as="button"
-            onClick={() => logout(true)}
-            title="Log out"
-            px={{ base: '0.75rem', md: '1rem' }}
-            borderRadius={{ base: 0, md: 'md' }}
-            mb="1rem"
-          >
-            {isMobile ? '' : 'Log out'}
           </SidebarItem>
         </SidebarContainer>
       </Flex>
