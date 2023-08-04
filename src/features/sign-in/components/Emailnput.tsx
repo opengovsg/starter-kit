4c69dd3033aa630ca9d124d596092f3f58cf26e6
Aiko Klostermann <aiko@open.gov.sg>
2023-08-04 19:03:00 +0800

    fix: make email addresses case insensetive
    to prevent multiple user accounts being created per eamil address.

--- src/features/sign-in/components/Emailnput.tsx
+++ src/features/sign-in/components/Emailnput.tsx
@@ -15,6 +15,7 @@ export const emailSignInSchema = z.object({
   email: z
     .string()
     .trim()
+    .toLowerCase()
     .min(1, 'Please enter an email address.')
     .email({ message: 'Please enter a valid email address.' }),
 })
