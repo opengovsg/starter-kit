4b4977020b3055cbc90511d505952dc0ff3645ce
Kar Rui Lau <karrui@open.gov.sg>
2023-08-10 13:24:56 +0800

    feat: use strict PublicPageWrapper on /sign-in
    will automatically redirect to home page if user is already signed in

--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -1,11 +1,9 @@
 import { Box, Flex, Skeleton, Text } from '@chakra-ui/react'
 import { RestrictedGovtMasthead } from '@opengovsg/design-system-react'
+import { PublicPageWrapper } from '~/components/AuthWrappers'
 
-import { noop } from 'lodash'
-import { useRouter } from 'next/router'
 import { RestrictedMiniFooter } from '~/components/RestrictedMiniFooter'
 import Suspense from '~/components/Suspense'
-import { CALLBACK_URL_KEY } from '~/constants/params'
 import { env } from '~/env.mjs'
 import {
   BackgroundBox,
@@ -17,82 +15,58 @@ import {
   SignInContextProvider,
   SignInForm,
 } from '~/features/sign-in/components'
-import { HOME } from '~/lib/routes'
-import { trpc } from '~/utils/trpc'
+import { type NextPageWithLayout } from '~/lib/types'
 
 const title = env.NEXT_PUBLIC_APP_NAME
 
-const SignIn = () => {
-  useRedirectIfSignedIn()
-
+const SignIn: NextPageWithLayout = () => {
   return (
-    <BackgroundBox>
-      <RestrictedGovtMasthead
-      // This component can only be used if this is an application created by OGP.
-      />
-      <BaseGridLayout flex={1}>
-        <NonMobileSidebarGridArea>
-          <LoginImageSvgr maxW="100%" aria-hidden />
-        </NonMobileSidebarGridArea>
-        <LoginGridArea>
-          <Box minH={{ base: 'auto', lg: '17.25rem' }} w="100%">
-            <Flex mb={{ base: '2.5rem', lg: 0 }} flexDir="column">
-              <Text
-                display={{ base: 'none', lg: 'initial' }}
-                textStyle="responsive-heading.heavy-1280"
-                mb="2.5rem"
-              >
-                {title}
-              </Text>
-              <Box display={{ base: 'initial', lg: 'none' }}>
-                <Box mb={{ base: '0.75rem', lg: '1.5rem' }}>
-                  <Text textStyle="h3">{title}</Text>
+    <PublicPageWrapper strict>
+      <BackgroundBox>
+        <RestrictedGovtMasthead
+        // This component can only be used if this is an application created by OGP.
+        />
+        <BaseGridLayout flex={1}>
+          <NonMobileSidebarGridArea>
+            <LoginImageSvgr maxW="100%" aria-hidden />
+          </NonMobileSidebarGridArea>
+          <LoginGridArea>
+            <Box minH={{ base: 'auto', lg: '17.25rem' }} w="100%">
+              <Flex mb={{ base: '2.5rem', lg: 0 }} flexDir="column">
+                <Text
+                  display={{ base: 'none', lg: 'initial' }}
+                  textStyle="responsive-heading.heavy-1280"
+                  mb="2.5rem"
+                >
+                  {title}
+                </Text>
+                <Box display={{ base: 'initial', lg: 'none' }}>
+                  <Box mb={{ base: '0.75rem', lg: '1.5rem' }}>
+                    <Text textStyle="h3">{title}</Text>
+                  </Box>
                 </Box>
-              </Box>
 
-              <SignInContextProvider>
-                <Suspense fallback={<Skeleton w="100vw" h="100vh" />}>
-                  <SignInForm />
-                </Suspense>
-              </SignInContextProvider>
-            </Flex>
-          </Box>
-        </LoginGridArea>
-      </BaseGridLayout>
-      <BaseGridLayout
-        bg={{ base: 'base.canvas.brandLight', lg: 'transparent' }}
-      >
-        <FooterGridArea>
-          <RestrictedMiniFooter
-          // This component can only be used if this is an application created by OGP.
-          />
-        </FooterGridArea>
-      </BaseGridLayout>
-    </BackgroundBox>
+                <SignInContextProvider>
+                  <Suspense fallback={<Skeleton w="100vw" h="100vh" />}>
+                    <SignInForm />
+                  </Suspense>
+                </SignInContextProvider>
+              </Flex>
+            </Box>
+          </LoginGridArea>
+        </BaseGridLayout>
+        <BaseGridLayout
+          bg={{ base: 'base.canvas.brandLight', lg: 'transparent' }}
+        >
+          <FooterGridArea>
+            <RestrictedMiniFooter
+            // This component can only be used if this is an application created by OGP.
+            />
+          </FooterGridArea>
+        </BaseGridLayout>
+      </BackgroundBox>
+    </PublicPageWrapper>
   )
-}
-
-// This hook is only meant to be used in `sign-in` page
-function useRedirectIfSignedIn() {
-  const router = useRouter()
-
-  const callbackUrl =
-    router.query[CALLBACK_URL_KEY] !== undefined
-      ? String(router.query[CALLBACK_URL_KEY])
-      : HOME
-
-  const { isLoading } = trpc.me.get.useQuery(undefined, {
-    // Just stay on this page on error
-    onError: noop,
-    onSuccess: async () => {
-      await router.push(callbackUrl)
-    },
-    // This is intentionally set to false for sign in page since we do not want the root ErrorBoundary to catch unauthorized errors
-    // If we do catch it, there will be an infinite redirect to `/sign-in`
-    useErrorBoundary: false,
-  })
-
-  return { isLoading }
 }
 
 export default SignIn
