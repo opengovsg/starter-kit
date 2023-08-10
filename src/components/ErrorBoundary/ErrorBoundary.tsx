70df5910c14b52814e658c7ab96dcdea8749bcfe
Kar Rui Lau <karrui@open.gov.sg>
2023-08-10 14:20:11 +0800

    feat: remove redirection to sign in page from error boundary
    non-obvious flow

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -7,7 +7,6 @@ import {
 } from './ErrorBoundary.types'
 import { TRPCWithErrorCodeSchema } from '../../utils/error'
 import { UnexpectedErrorCard } from './UnexpectedErrorCard'
-import { CALLBACK_URL_KEY } from '~/constants/params'
 
 /**
  * Does the following:
@@ -46,31 +45,12 @@ class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
 
     if (!res.success) return fallback ?? <UnexpectedErrorCard />
 
-    // The choice to not redirect via next's router was intentional to handle ErrorBoundary for the app root
-    // Using next's router.push('/sign-in') will not render the SignIn component as it won't be mounted in the app root as the ErrorBoundary fallback component will be rendered instead
-    // Using vanilla location redirecting will prompt a full page reload of /sign-in page, which will never trigger the root ErrorBoundary, thus rendering the full component correctly
-    if (res.data === 'UNAUTHORIZED') {
-      const params = new URLSearchParams(window.location.search)
-
-      const callbackUrl = params.get('callbackUrl')
-
-      window.location.href = !!callbackUrl
-        ? `/sign-in/?${CALLBACK_URL_KEY}=${callbackUrl}`
-        : `/sign-in`
-
-      return
-    }
-
     return fallback ?? <ErrorComponent code={res.data} />
   }
 }
 
 // TODO: Make custom components for these
-function ErrorComponent({
-  code,
-}: {
-  code: Exclude<TRPC_ERROR_CODE_KEY, 'UNAUTHORIZED'>
-}) {
+function ErrorComponent({ code }: { code: TRPC_ERROR_CODE_KEY }) {
   switch (code) {
     case 'NOT_FOUND':
       return (
