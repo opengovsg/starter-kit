be5ba9f641ce7ac940c8ab996a27300c666e2a9f
Kar Rui Lau <karrui@open.gov.sg>
2023-08-10 13:30:33 +0800

    feat: clear localStorage when logout is invoked manually

--- src/features/me/api/useMe.ts
+++ src/features/me/api/useMe.ts
@@ -1,28 +1,27 @@
 import { useCallback } from 'react'
 import Router from 'next/router'
 import { trpc } from '~/utils/trpc'
+import { useLocalStorage } from 'usehooks-ts'
+import { LOGGED_IN_KEY } from '~/constants/localStorage'
 
-export const useMe = ({ redirectTo = '', redirectIfFound = false } = {}) => {
-  const [me] = trpc.me.get.useSuspenseQuery(undefined, {
-    onSuccess: async () => {
-      if (redirectIfFound) {
-        await Router.push(redirectTo)
-      }
-    },
-  })
+export const useMe = () => {
+  const [, setIsAuthenticated] = useLocalStorage<boolean>(LOGGED_IN_KEY, false)
+  const [me] = trpc.me.get.useSuspenseQuery()
+
   const logoutMutation = trpc.auth.logout.useMutation()
 
   const logout = useCallback(
     (redirectToSignIn = true) => {
       return logoutMutation.mutate(undefined, {
         onSuccess: async () => {
+          setIsAuthenticated(false)
           if (redirectToSignIn) {
             await Router.push('/sign-in')
           }
         },
       })
     },
-    [logoutMutation]
+    [logoutMutation, setIsAuthenticated]
   )
 
   return { me, logout }
