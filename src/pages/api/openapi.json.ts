1de4f8f49b5c5133ffbfede47093f44bb88734f5
LoneRifle <LoneRifle@users.noreply.github.com>
2023-10-10 15:54:22 +0800

    fix(openapi): send spec with `.json()`, use cookie auth

--- src/pages/api/openapi.json.ts
+++ src/pages/api/openapi.json.ts
@@ -4,7 +4,7 @@ import { openApiDocument } from '../../server/openapi'
 
 // Respond with our OpenAPI schema
 const handler = (_req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).send(openApiDocument)
+  res.status(200).json(openApiDocument)
 }
 
 export default handler
--- src/server/openapi.ts
+++ src/server/openapi.ts
@@ -1,9 +1,17 @@
 import { generateOpenApiDocument } from 'trpc-openapi'
 import { appRouter } from './modules/_app'
 import { getBaseUrl } from '~/utils/getBaseUrl'
+import { sessionOptions } from '~/lib/auth'
 
 export const openApiDocument = generateOpenApiDocument(appRouter, {
   title: 'Starter Kit OpenAPI',
   version: '1.0.0',
   baseUrl: `${getBaseUrl()}/api`,
+  securitySchemes: {
+    cookieAuth: {
+      type: 'apiKey',
+      in: 'cookie',
+      name: sessionOptions.cookieName,
+    },
+  },
 })
