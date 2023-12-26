9b57012126907ea38a5381ef71b05cef6e2ceae5
Dexter Tan <dev@dextertanyj.com>
2023-12-26 11:17:04 +0000

    chore: add accessibility plugin for storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,6 +6,7 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-themes',
+    '@storybook/addon-a11y',
   ],
   framework: {
     name: '@storybook/nextjs',
