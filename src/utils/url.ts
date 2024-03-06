a2fceaa4d2fb071bc647ccd34aeed27659b0467c
Kar Rui Lau <karrui@open.gov.sg>
2024-03-06 17:42:42 +0800

    fix: use origin check to ensure relative URL

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,5 +1,6 @@
 import { type ParsedUrlQuery } from 'querystring'
 import { CALLBACK_URL_KEY } from '~/constants/params'
+import { getBaseUrl } from './getBaseUrl'
 
 export const appendWithRedirect = (url: string, redirectUrl?: string) => {
   if (!redirectUrl || !isRelativeUrl(redirectUrl)) {
@@ -15,19 +16,12 @@ export const getRedirectUrl = (query: ParsedUrlQuery) => {
   return decodeURIComponent(String(query[CALLBACK_URL_KEY]))
 }
 
-// Scheme: https://tools.ietf.org/html/rfc3986#section-3.1
-// Absolute URL: https://tools.ietf.org/html/rfc3986#section-4.3
-// Adapted from https://github.com/sindresorhus/is-absolute-url/blob/main/index.js
 export const isRelativeUrl = (url: string) => {
-  const ABSOLUTE_URL_REGEX = /^[a-zA-Z][a-zA-Z\d+\-.]*?:/
-  const PROTOCOL_RELATIVE_URL_REGEX = /^\/\//
-
-  // Windows paths like `c:\`
-  const WINDOWS_PATH_REGEX = /^[a-zA-Z]:\\/
-
-  if (WINDOWS_PATH_REGEX.test(url)) {
+  const baseUrl = getBaseUrl()
+  try {
+    const normalizedUrl = new URL(url, baseUrl)
+    return new URL(baseUrl).origin === normalizedUrl.origin
+  } catch (e) {
     return false
   }
-
-  return !ABSOLUTE_URL_REGEX.test(url) && !PROTOCOL_RELATIVE_URL_REGEX.test(url)
 }
