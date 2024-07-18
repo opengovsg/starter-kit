92ecf63b07743fecae78ca22ca39f1f24718c2dc
Zeyu Zhang <39144422+zeyu2001@users.noreply.github.com>
2024-07-18 18:01:18 +0800

    fix: validate callback urls with starter-kitty validator

--- src/schemas/url.ts
+++ src/schemas/url.ts
@@ -1,10 +1,31 @@
 import { z } from 'zod'
 import { HOME } from '~/lib/routes'
-import { isRelativeUrl } from '~/utils/url'
+import { UrlValidator } from '@opengovsg/starter-kitty-validators'
+import { getBaseUrl } from '~/utils/getBaseUrl'
+
+const baseUrl = getBaseUrl()
+
+const validator = new UrlValidator({
+  baseOrigin: new URL(baseUrl).origin,
+  whitelist: {
+    protocols: ['http', 'https'],
+    hosts: [new URL(baseUrl).host],
+  },
+})
 
 export const callbackUrlSchema = z
   .string()
   .optional()
   .default(HOME)
-  .refine((url) => url && isRelativeUrl(url))
-  .catch(HOME)
+  .transform((url, ctx) => {
+    try {
+      return validator.parse(url)
+    } catch (error) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: (error as Error).message,
+      })
+      return z.NEVER
+    }
+  })
+  .catch(new URL(HOME, baseUrl))
