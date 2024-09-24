64af40f56799c800f50c89b7c733063ec58e5559
Kar Rui Lau <karrui@open.gov.sg>
2024-09-24 16:59:52 +0800

    feat(ci): try using istanbul instead for coverage
    v8 doesn't seem to pipe to dd

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     },
     setupFiles: ['vitest.setup.ts'],
     coverage: {
-      provider: 'v8',
+      provider: 'istanbul',
       reportOnFailure: true,
     },
   },
