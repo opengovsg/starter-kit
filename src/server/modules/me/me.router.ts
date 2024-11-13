62621199af9881b1901fc51c3c13e3a5e15db45e
Kar Rui Lau <karrui@open.gov.sg>
2024-11-13 16:03:11 +0800

    feat: remove redundant db query in me.get (#352)

--- src/server/modules/me/me.router.ts
+++ src/server/modules/me/me.router.ts
@@ -3,13 +3,11 @@
  * This is an example router, you can delete this file and then update `../pages/api/trpc/[trpc].tsx`
  */
 import { protectedProcedure, router } from '~/server/trpc'
-import { defaultMeSelect } from './me.select'
 
 export const meRouter = router({
   get: protectedProcedure.query(async ({ ctx }) => {
-    return await ctx.prisma.user.findUniqueOrThrow({
-      where: { id: ctx.user.id },
-      select: defaultMeSelect,
-    })
+    // Remember to write a new query if you want to return something different than what
+    // the authMiddleware in protectedProcedure returns.
+    return ctx.user
   }),
 })
