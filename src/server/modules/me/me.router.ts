523b87a12537efe5cd76791da1d3d44f69703e71
Kar Rui Lau <karrui@open.gov.sg>
2024-11-15 11:30:48 +0800

    feat: add openapi example

--- src/server/modules/me/me.router.ts
+++ src/server/modules/me/me.router.ts
@@ -2,12 +2,31 @@
  *
  * This is an example router, you can delete this file and then update `../pages/api/trpc/[trpc].tsx`
  */
+import { z } from 'zod'
+
 import { protectedProcedure, router } from '~/server/trpc'
 
 export const meRouter = router({
-  get: protectedProcedure.query(async ({ ctx }) => {
-    // Remember to write a new query if you want to return something different than what
-    // the authMiddleware in protectedProcedure returns.
-    return ctx.user
-  }),
+  get: protectedProcedure
+    .meta({
+      openapi: {
+        protect: true,
+        method: 'GET',
+        path: '/users/me',
+      },
+    })
+    .input(z.object({}).optional())
+    .output(
+      z.object({
+        id: z.string(),
+        email: z.string(),
+        name: z.string().nullable(),
+        image: z.string().nullable(),
+      }),
+    )
+    .query(async ({ ctx }) => {
+      // Remember to write a new query if you want to return something different than what
+      // the authMiddleware in protectedProcedure returns.
+      return ctx.user
+    }),
 })
--- src/server/swagger.ts
+++ src/server/swagger.ts
@@ -2,9 +2,17 @@ import { generateOpenApiDocument } from 'trpc-swagger'
 
 import { getBaseUrl } from '~/utils/getBaseUrl'
 import { appRouter } from './modules/_app'
+import { sessionOptions } from './modules/auth/session'
 
 export const openApiDocument = generateOpenApiDocument(appRouter, {
   title: 'tRPC Swagger',
   version: '1.0.0', // consider making this pull version from package.json
   baseUrl: `${getBaseUrl()}/api`,
+  securitySchemes: {
+    ironSession: {
+      type: 'apiKey',
+      in: 'cookie',
+      name: sessionOptions.cookieName,
+    },
+  },
 })
