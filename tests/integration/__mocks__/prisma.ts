3f0df273479df7fb163e2f326d8ab346dc57ec51
Kar Rui Lau <karrui@open.gov.sg>
2025-03-25 17:00:15 +0800

    fix(test): remove logging of test database conn string

--- tests/integration/__mocks__/prisma.ts
+++ tests/integration/__mocks__/prisma.ts
@@ -8,8 +8,6 @@ import { getDatabaseUrl, hash, integreSQL } from '../helpers/hooks'
 vi.mock('~/server/prisma', async () => {
   const testDb = await integreSQL.getTestDatabase(hash)
   const connUrl = await getDatabaseUrl(testDb)
-  console.log('Connecting to test database:', connUrl)
-
   return {
     prisma: new PrismaClient({
       datasources: { db: { url: connUrl } },
