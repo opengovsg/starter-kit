ab1039eae3b95938d58f7759615a69b07b4f1bc0
Kishore <42832651+kishore03109@users.noreply.github.com>
2025-07-25 09:40:34 +0800

    fix: use monotonic clock for measuring spans (#475)

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -45,14 +45,14 @@ const t = initTRPC.context<Context>().create({
 // Setting outer context with tRPC will not get us correct path during request batching,
 // only by setting logger context in the middleware do we get the exact path to log
 const loggerMiddleware = t.middleware(async ({ path, next, ctx, type }) => {
-  const start = Date.now()
+  const start = performance.now()
   const logger = createBaseLogger({ path, clientIp: getIP(ctx.req) })
 
   const result = await next({
     ctx: { logger },
   })
 
-  const durationInMs = Date.now() - start
+  const durationInMs = Math.round(performance.now() - start)
 
   if (result.ok) {
     logger.info({ durationInMs }, `[${type}]: ${path} - ${durationInMs}ms - OK`)
