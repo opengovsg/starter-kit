3954074f7a7f88153c009acb2a8fffaa5003a243
Kar Rui Lau <karrui@open.gov.sg>
2025-10-07 11:11:14 +0800

    feat: add tooling packages

---
+++ tooling/prettier/index.js
@@ -0,0 +1,44 @@
+import * as sortImportsPlugin from "@ianvs/prettier-plugin-sort-imports";
+import * as tailwindPlugin from "prettier-plugin-tailwindcss";
+
+/** @typedef {import("prettier").Config} PrettierConfig */
+/** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
+/** @typedef {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
+
+/** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
+const config = {
+  plugins: [sortImportsPlugin, tailwindPlugin],
+  tailwindFunctions: ["cn", "cva"],
+  importOrder: [
+    "<TYPES>",
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "<TYPES>^@acme",
+    "^@acme/(.*)$",
+    "",
+    "<TYPES>^[.|..|~]",
+    "^~/",
+    "^[../]",
+    "^[./]",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderTypeScriptVersion: "5.0.0",
+  overrides: [
+    {
+      files: "*.json.hbs",
+      options: {
+        parser: "json",
+      },
+    },
+    {
+      files: "*.js.hbs",
+      options: {
+        parser: "babel",
+      },
+    },
+  ],
+};
+
+export default config;
