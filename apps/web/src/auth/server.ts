7977d4d98b7c9b164058dd91623e40198b220463
Kar Rui Lau <karrui@open.gov.sg>
2025-10-08 17:08:21 +0800

    feat: setup initial (non-working) web app

---
+++ apps/web/src/auth/server.ts
@@ -0,0 +1,27 @@
+import 'server-only'
+
+import { cache } from 'react'
+import { headers } from 'next/headers'
+
+import { initAuth } from '@acme/auth'
+
+import { env } from '~/env'
+
+const baseUrl =
+  env.VERCEL_ENV === 'production'
+    ? `https://${env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : env.VERCEL_ENV === 'preview'
+      ? `https://${env.VERCEL_URL}`
+      : 'http://localhost:3000'
+
+export const auth = initAuth({
+  baseUrl,
+  productionUrl: `https://${env.VERCEL_PROJECT_PRODUCTION_URL ?? 'turbo.t3.gg'}`,
+  secret: env.AUTH_SECRET,
+  discordClientId: env.AUTH_DISCORD_ID,
+  discordClientSecret: env.AUTH_DISCORD_SECRET,
+})
+
+export const getSession = cache(async () =>
+  auth.api.getSession({ headers: await headers() }),
+)
