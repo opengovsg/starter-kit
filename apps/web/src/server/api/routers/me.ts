51a6d1e84f2e4daadd28f85f5a373dfd1e77a1a0
Kar Rui Lau <karrui@open.gov.sg>
2025-10-10 14:56:37 +0800

    feat: update meRouter

--- apps/web/src/server/api/routers/me.ts
+++ apps/web/src/server/api/routers/me.ts
@@ -1,9 +1,9 @@
 import { createTRPCRouter, protectedProcedure } from '~/server/api/trpc'
 
 export const meRouter = createTRPCRouter({
-  get: protectedProcedure.query(() => {
+  get: protectedProcedure.query(({ ctx }) => {
     // Remember to write a new query if you want to return something different than what
     // the authMiddleware in protectedProcedure returns.
-    return 'TODO: return user info here from ctx.session'
+    return ctx.session.userId
   }),
 })
