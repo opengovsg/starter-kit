6dcc9daf0d793fcb174c44a427a320b18193b6c3
seaerchin <jiachin@open.gov.sg>
2025-11-20 17:01:56 +0800

    chore: add eslint rule

--- packages/db/eslint.config.js
+++ packages/db/eslint.config.js
@@ -3,6 +3,18 @@ import baseConfig from '@acme/eslint-config/base'
 /** @type {import('typescript-eslint').Config} */
 export default [
   {
+    // NOTE: we should ban accesses on the `transaction` property for kysely
+    // because this doesn't work well with the plugin
+    rules: {
+      'no-restricted-syntax': [
+        'error',
+        {
+          selector:
+            "MemberExpression[property.name='transaction'][object.property.name='$kysely']",
+          message: 'Use db.$transaction() instead of db.$kysely.transaction()',
+        },
+      ],
+    },
     ignores: ['dist/**', 'src/generated/**'],
   },
   ...baseConfig,
