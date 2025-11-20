5a3589b89b7371213a1c4b91686f8c54817c82a6
seaerchin <jiachin@open.gov.sg>
2025-11-20 15:22:15 +0800

    chore: fix eslint and pg volume configs

--- packages/db/eslint.config.js
+++ packages/db/eslint.config.js
@@ -3,7 +3,7 @@ import baseConfig from '@acme/eslint-config/base'
 /** @type {import('typescript-eslint').Config} */
 export default [
   {
-    ignores: ['dist/**', 'generated'],
+    ignores: ['dist/**', 'src/generated/**'],
   },
   ...baseConfig,
 ]
