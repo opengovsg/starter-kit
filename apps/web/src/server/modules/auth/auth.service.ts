57fc0b84b1f01b08cd7ee4b507f3cd375a79b0db
seaerchin <jiachin@open.gov.sg>
2025-11-25 19:24:19 +0900

    fix: throw trpc error if unable to delete
    since we pass all the checks, if we can't delete the token, we just
    throw a trpc error to feedabck to the user

--- apps/web/src/server/modules/auth/auth.service.ts
+++ apps/web/src/server/modules/auth/auth.service.ts
@@ -119,9 +119,19 @@ export const emailVerifyOtp = async ({
       .deleteFrom('VerificationToken')
       .where('identifier', '=', vfnIdentifier)
       .returningAll()
-      .executeTakeFirstOrThrow()
+      .executeTakeFirstOrThrow(
+        // NOTE: If we are unable to find the token,
+        // this means that it has been deleted between
+        // our initial query and the deletion here
+        () =>
+          new TRPCError({
+            code: 'BAD_REQUEST',
+            message:
+              'Token is invalid or has expired. Please request a new OTP.',
+          }),
+      )
   } catch (error) {
-    // see error code here: https://www.prisma.io/docs/reference/api-reference/error-reference#p2025
+    // see error code here: https://www.prisma.io/docs/orm/reference/error-reference#p2025
     if (
       error instanceof Prisma.PrismaClientKnownRequestError &&
       error.code === 'P2025'
