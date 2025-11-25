c3f1a6cbfb7625ea94758b93e27fa23027386823
seaerchin <jiachin@open.gov.sg>
2025-11-25 17:49:03 +0900

    fix: update lint config to ignore generated files

--- packages/db/eslint.config.js
+++ packages/db/eslint.config.js
@@ -3,6 +3,9 @@ import baseConfig from '@acme/eslint-config/base'
 /** @type {import('typescript-eslint').Config} */
 export default [
   {
+    ignores: ['src/generated/**', 'dist/**'],
+  },
+  {
     // NOTE: we should ban accesses on the `transaction` property for kysely
     // because this doesn't work well with the plugin
     rules: {
@@ -15,7 +18,6 @@ export default [
         },
       ],
     },
-    ignores: ['dist/**', 'src/generated/**'],
   },
   ...baseConfig,
 ]
