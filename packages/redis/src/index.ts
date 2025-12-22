afcc2662e8a0d9384901dca29a973ddf917de6a3
Kar Rui Lau <karrui@open.gov.sg>
2025-12-22 19:18:41 +0800

    feat: add warning for cache_hostname not being set

--- packages/redis/src/index.ts
+++ packages/redis/src/index.ts
@@ -8,6 +8,9 @@ const globalForRedis = global as unknown as {
 
 const createRedisClient = (): Redis | null => {
   if (!env.CACHE_HOSTNAME) {
+    console.warn(
+      '!!!! CACHE_HOSTNAME is not set, Redis client will not be created. !!!!',
+    )
     return null
   }
 
