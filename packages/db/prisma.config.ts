e1d33c763d554d7017f8c10d002f76e81430e735
Kar Rui Lau <karrui@open.gov.sg>
2026-01-06 13:59:46 +0800

    feat(db): revert change to use prisma config env
    still dont work with undefined

--- packages/db/prisma.config.ts
+++ packages/db/prisma.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig, env } from 'prisma/config'
+import { defineConfig } from 'prisma/config'
 
 export default defineConfig({
   migrations: {
     seed: 'pnpm dlx tsx prisma/seed.ts',
   },
   datasource: {
-    url: env('DATABASE_URL'),
+    // Not using `env` from 'prisma/config' to prevent errors thrown when
+    // DATABASE_URL is not set (e.g. prisma generate).
+    // See https://github.com/prisma/prisma/issues/28590
+    url: process.env.DATABASE_URL,
   },
   schema: 'prisma/schema.prisma',
 })
