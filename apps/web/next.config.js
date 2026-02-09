3fbae73898393e999201f06cab6ba2aebd9bf9e3
Kar Rui Lau <karrui@open.gov.sg>
2026-02-09 14:54:37 +0800

    feat: add docs to next.config.js body size limit

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -8,6 +8,9 @@ await jiti.import('./src/env')
 /** @type {import("next").NextConfig} */
 const config = {
   experimental: {
+    // Limits body size in our endpoints.
+    // Only affects route matches in proxy.ts, so you must be careful to not remove matches in that file
+    // or the limit can be bypassed and cause OOM server crashes.
     proxyClientMaxBodySize: '2mb',
   },
   /** Enables hot reloading for local packages without a build step */
