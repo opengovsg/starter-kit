ef435c917aa1d31b17c2ef2b1cac10920027f760
Kar Rui Lau <karrui@open.gov.sg>
2026-02-09 14:46:11 +0800

    feat: add function to help generate csp header

--- apps/web/src/proxy.ts
+++ apps/web/src/proxy.ts
@@ -1,30 +1,88 @@
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 
-import { env } from '~/env'
+interface CspPolicy {
+  'default-src'?: string[]
+  'script-src'?: string[]
+  'style-src'?: string[]
+  'font-src'?: string[]
+  'object-src'?: string[]
+  'base-uri'?: string[]
+  'img-src'?: string[]
+  'form-action'?: string[]
+  'frame-src'?: string[]
+  'frame-ancestors'?: string[]
+  'connect-src'?: string[]
+}
+
+/**
+ * Merges multiple security policies into a single policy string.
+ */
+function generateCspHeader(policies: CspPolicy[]): string {
+  const combined = policies.reduce((combined, policy) => {
+    Object.keys(policy).forEach((_directive) => {
+      const directive = _directive as keyof CspPolicy
+      const sources = Array.from(
+        new Set([...(combined[directive] ?? []), ...(policy[directive] ?? [])]),
+      )
+      combined[directive] = sources
+    })
+
+    return combined
+  }, {})
+
+  const baseDirectives = Object.entries(combined).map(
+    ([directive, sources]) =>
+      `${directive} ${(sources as string[]).sort().join(' ')}`,
+  )
+
+  return [...baseDirectives, 'upgrade-insecure-requests'].join('; ')
+}
+
+const defaultPolicy: CspPolicy = {
+  'default-src': ["'self'"],
+  'script-src': ["'self'", "'strict-dynamic'"],
+  'connect-src': ["'self'"],
+  'style-src': ["'self'", "'unsafe-inline'"],
+  'img-src': ["'self'", 'data:', 'blob:'],
+  'font-src': ["'self'"],
+  'base-uri': ["'self'"],
+  'form-action': ["'self'"],
+  'frame-ancestors': ["'none'"],
+  'frame-src': ["'self'"],
+}
+
+const vercelLivePolicy = {
+  'connect-src': [
+    'https://vercel.live',
+    'https://*.pusher.com',
+    'wss://*.pusher.com',
+  ],
+  'img-src': ['https://vercel.com'],
+  'script-src': ['https://vercel.live'],
+  'style-src': ['https://vercel.live'],
+  'font-src': ['https://vercel.live'],
+  'frame-src': ['https://vercel.live'],
+}
 
 export function proxy(request: NextRequest) {
-  const isDev = env.NODE_ENV === 'development' || env.NODE_ENV === 'test'
+  // eslint-disable-next-line no-restricted-properties
+  const isVercelPreview = process.env.VERCEL_ENV === 'preview'
+  const isDev =
+    // eslint-disable-next-line no-restricted-properties
+    process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'
 
   // Read more on how to use this nonce with custom scripts:
   // https://nextjs.org/docs/app/guides/content-security-policy#reading-the-nonce
   const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
 
   // Update the headers as required, e.g. to allow Datadog RUM, Vercel Insights, Google Analytics, etc.
-  const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${isDev ? "'unsafe-eval'" : ''};
-    style-src 'self';
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    img-src * data: blob:;
-    form-action 'self';
-    frame-src *;
-    frame-ancestors *;
-    connect-src 'self' https://vitals.vercel-insights.com/v1/vitals;
-    ${isDev ? '' : 'upgrade-insecure-requests;'}
-`
+  const cspHeader = generateCspHeader([
+    defaultPolicy,
+    { 'script-src': [`'nonce-${nonce}'`] },
+    isVercelPreview ? vercelLivePolicy : {},
+    isDev ? { 'script-src': ["'unsafe-eval'"] } : {},
+  ])
 
   // Replace newline characters and spaces
   const contentSecurityPolicyHeaderValue = cspHeader
