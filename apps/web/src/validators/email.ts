54161201f36a5058fe363afa0d0daf880adbe73c
Ken Lee Shu Ming <leeshumingken@hotmail.com>
2026-03-10 11:49:03 +0800

    feat: add max length validation for email addresses (#567)
    Added a maximum length validation for email addresses to prevent overly large inputs.

--- apps/web/src/validators/email.ts
+++ apps/web/src/validators/email.ts
@@ -4,6 +4,9 @@ import z from 'zod'
 export const emailSchema = z
   .string()
   .min(1, 'Please enter an email address.')
+  // https://www.rfc-editor.org/rfc/rfc5321#section-4.5.3.1.3 defines that emails should not be more than 256
+  // z.email doesn't have a maximum either, this may cause the regex to try eval-ing a very large payload
+  .max(256, 'Please enter a valid email address.')
   .check(
     z.trim(),
     z.email({ error: 'Please enter a valid email address.' }),
